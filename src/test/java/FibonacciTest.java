2cc948848dee120da0e1df32a7a4883bc1b27021
Eliezer <reuvene@mprest.com>
2020-09-27 10:57:13 +0300

    cuma refactoring tests for Junit 5 parameterized

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,66 +1,53 @@
 import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 public class FibonacciTest {
 
-    @Test
-    public void testFib0() {
-        testFib(0, 0);
-    }
-
-    @Test
-    public void testFib1() {
-        testFib(1, 1);
-    }
-
-    @Test
-    public void testFib2() {
-        testFib(1, 2);
-    }
-
-    @Test
-    public void testFib3() {
-        testFib(2, 3);
-    }
-
-    @Test
-    public void testFib4() {
-        testFib(3, 4);
-    }
-
-    @Test
-    public void testFib5() {
-        testFib(5, 5);
-    }
-
-    @Test
-    public void testFib2000000() throws IOException {
-        String expectedPath = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "fibonacci_result_2000000.txt";
-        String expectedValue = FileUtils.readFileToString(new File(expectedPath), StandardCharsets.UTF_8);
-        testFib(expectedValue, 2000000);
-    }
-
-    private static void testFib(long expected, long input) {
-        testFib(String.valueOf(expected), input);
-    }
-
-    private static void testFib(String expected, long input) {
+    @ParameterizedTest()
+    @MethodSource({"fibArgumentsProvider"})
+    public void testFib(String expected, long input) {
         BigInteger found = null;
         try {
             found = Fibonacci.fib(input);
         } catch (Throwable e) {
-            Assert.fail("exception during test: " + e);
+            Assertions.fail("exception during test: " + e);
         }
         if (found == null)
-            Assert.fail();
-        Assert.assertEquals(expected, found.toString());
+            Assertions.fail();
+        Assertions.assertEquals(expected, found.toString());
     }
 
+    private static Stream<Arguments> fibArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(String.valueOf(0), 0),
+                Arguments.of(String.valueOf(1), 1),
+                Arguments.of(String.valueOf(1), 2),
+                Arguments.of(String.valueOf(2), 3),
+                Arguments.of(String.valueOf(3), 4),
+                Arguments.of(String.valueOf(5), 5),
+                Arguments.of("7896325826131730509282738943634332893686268675876375", 250),
+                Arguments.of(readTooLongStr("fibonacci_result_2000000.txt"), 2000000)
+        );
+    }
+
+    private static String readTooLongStr(String filePath) {
+        String expectedPath = "src" + File.separator + "test" + File.separator + "resources" + File.separator + filePath;
+        String expectedValue = null;
+        try {
+            expectedValue = FileUtils.readFileToString(new File(expectedPath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return expectedValue;
+    }
 
 }
